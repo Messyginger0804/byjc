140ddfa3c8390267c5c5147bcd71b115f68df79e
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2025-01-29 20:05:50 +0000

    rearranged the blogs for the features and the blog of the month

--- src/components/Home/FeatuedPosts.js
+++ src/components/Home/FeatuedPosts.js
@@ -24,14 +24,14 @@ function FeatuedPosts({ blogs }) {
 
             <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
                 <article className=" col-span-2 sxl:col-span-1 row-span-2 relative">
-                    <BlogLayoutOne blog={sortedBlogs[12]} />
+                    <BlogLayoutOne blog={sortedBlogs[5]} />
                 </article>
                 <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
                     <BlogLayoutTwo blog={sortedBlogs[1]} />
 
                 </article>
                 <article className="col-span-2 sm:col-span-1 row-span-1 relative">
-                    <BlogLayoutTwo blog={sortedBlogs[6]} />
+                    <BlogLayoutTwo blog={sortedBlogs[9]} />
                 </article>
 
             </div>
