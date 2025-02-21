39922acdf8069a651bb03d6544747c6474276659
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2025-02-21 18:02:20 +0000

    updated professional array

--- src/components/Portfolio/ProfessionalCard.js
+++ src/components/Portfolio/ProfessionalCard.js
@@ -3,24 +3,42 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { truncateText } from '../../data/utilities';
 
 export function ProfessionalCard({ job }) {
+    const maxDescLength = 150;
+
     return (
         <div className='text-center p-6 rounded-xl w-[280px] h-[400px] my-6 max-w-sm relative shadow-accent dark:shadow-accentDark
          hover:shadow-xl text-black bg-accent/90 dark:bg-accentDark dark:border-light dark:text-black border-2 border-black hover:scale-105 duration-200'>
-            <h3 className='text-lg font-medium dark:text-black pt-8 pb-2'>{job.title}</h3>
-            <p><span className='px-2'>client:</span>{job.client}</p>
-            <a href={`tel:${job.phoneNumber}`} className='hover:underline'>{job.phoneNumber}</a>
-            <Link href={job.link}>
-                <div className='flex justify-center'>
-                    <Image
-                        className='flex justify-center fit object-center rounded h-28 w-28 rounded-xl'
-                        src={job.image}
-                        alt={`${job.title} 'logo'`}
-                    />
+
+            <h3 className='text-lg font-medium dark:text-black absolute top-4 left-0 right-0'>{job.title}</h3>
+
+            <div className='flex flex-col justify-between h-full pt-12'>
+                <div className='flex justify-center h-[100px]'>
+                    <Link target='_blank' href={job.link}>
+                        <Image
+                            className='fit object-center rounded'
+                            src={job.image}
+                            width={100}
+                            height={100}
+                            alt={`${job.title} logo`}
+                        />
+                    </Link>
                 </div>
-            </Link>
-            <h4 className='py-4 text-black'>{job.description}</h4>
+
+                {/* Description container */}
+                <div className='h-[150px] my-2 '>
+                    <h3 className='text-lg font-medium pt-1 pb-1'>{job.descTitle}</h3>
+                    <p className='text-xs'>{truncateText(job.description, maxDescLength)}</p>
+                </div>
+
+                {/* Contact Info */}
+                {job.client && <p className='text-xs'><span className='px-2'>Client:</span>{job.client}</p>}
+                {job.phoneNumber && (
+                    <a target='_blank' href={`tel:${job.phoneNumber}`} className='text-xs hover:underline'>{job.phoneNumber}</a>
+                )}
+            </div>
         </div>
     );
 }
--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,6 +1,9 @@
-import JLD from '../components/Portfolio/assets/JLD.png'
-import TFLS from '../components/Portfolio/assets/TFLS.jpg'
-import ISW from '../components/Portfolio/assets/main_logo.png'
+import JLD from '../components/Portfolio/assets/JLD.png';
+import TFLS from '../components/Portfolio/assets/TFLS.jpg';
+import ISW from '../components/Portfolio/assets/main_logo.png';
+import swgr from '../../public/assets/logo.png';
+import alaskaninn from '../../public/assets/alaska-logo.png';
+import hush from '../../public/assets/HUSH-LOGO-white.png'
 
 export const contractedJobs = [
     {
@@ -9,15 +12,7 @@ export const contractedJobs = [
         phoneNumber: '646-535-8422',
         link: 'https://www.iservicewebsites.com/',
         image: ISW,
-        description: 'iServiceWeb',
-    },
-    {
-        title: 'Top Flight Lawn Service',
-        contact: 'Rene Guillen',
-        phoneNumber: '214-779-4259',
-        link: 'https://topflightlawnservice.vercel.app/',
-        image: TFLS,
-        description: 'DFW Landscaping Company',
+        description: 'iServiceWeb specializes in web dev and digital solutions, helping businesses establish a strong online presence with and innovative digital marketing strategies.',
     },
     {
         title: 'JLD Counseling',
@@ -25,6 +20,34 @@ export const contractedJobs = [
         phoneNumber: '214-785-7426',
         link: 'https://jldcounseling.com/',
         image: JLD,
-        description: 'DFW Therapy and Counseling Clinic',
+        description: 'JLD Counseling is a therapy and counseling clinic that provides compassionate and professional mental health services to individuals, couples, and families seeking guidance and support.',
     },
+    {
+        title: 'SWGR Management',
+        // No contact or phone number
+        link: 'https://swgrmanagement.com/',
+        image: swgr,
+        description: 'SWGR Management is a leading commercial real estate firm in the DFW area, offering a full range of property management services, from leasing and acquisitions to investment opportunities for businesses of all sizes.',
+    },
+    {
+        title: 'Alaska Inn',
+        // No contact or phone number
+        link: 'https://www.alaskaninn.com/',
+        image: alaskaninn,
+        description: 'Nestled in the breathtaking mountains of Utah, the Alaska Inn is a charming hotel and lodge offering a luxurious and secluded getaway. With cozy accommodations and stunning views, it’s the perfect destination for relaxation and adventure.',
+    },
+    {
+        title: 'Top Flight Lawn Service',
+        contact: 'Rene Guillen',
+        phoneNumber: '214-779-4259',
+        link: 'https://topflightlawnservice.vercel.app/',
+        image: TFLS,
+        description: 'Top Flight Lawn Service provides high-quality landscaping and lawn maintenance in the DFW area. Specializing in lawn care, garden design, and seasonal cleanup, they ensure that every yard looks pristine and well-maintained.',
+    },
+    // {
+    //     title: 'Hush Kratom',
+    //     link: 'https://hushkratom.com/',
+    //     image: hush,
+    //     description: `Hush Kratom is a premium brand specializing in high-quality kratom products, offering a variety of capsules, extracts, and powders sourced from the finest kratom leaves. Known for its potent and consistent formulations, Hush Kratom is a trusted name among kratom enthusiasts, providing natural wellness solutions while maintaining strict quality and safety standards. Whether you're seeking relaxation, focus, or energy, Hush Kratom delivers carefully crafted products designed to enhance your lifestyle.`
+    // },
 ];
