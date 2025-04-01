dfd22fe46bcd3927522d09ac518edbf9b3881913
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2025-04-01 20:33:21 +0000

    updated feature and home cover blog

--- src/components/Home/FeatuedPosts.js
+++ src/components/Home/FeatuedPosts.js
@@ -24,7 +24,7 @@ function FeatuedPosts({ blogs }) {
 
             <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
                 <article className=" col-span-2 sxl:col-span-1 row-span-2 relative">
-                    <BlogLayoutOne blog={sortedBlogs[6]} />
+                    <BlogLayoutOne blog={sortedBlogs[0]} />
                 </article>
                 <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
                     <BlogLayoutTwo blog={sortedBlogs[11]} />
